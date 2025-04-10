30cafa96efd9b114ebd7c8d67998735ddb131e85
max lazovsky <makslazovsky@ya.ru>
2025-04-11 01:41:13 +0300

    Docker configuration

---
+++ Domain/Entities/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Persistence/Contexts/EventAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Persistence.Contexts
+{
+    public class EventAppDbContext : DbContext
+    {
+        public EventAppDbContext(DbContextOptions<EventAppDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Event> Events => Set<Event>(); // Пример: если у тебя уже есть сущность Event
+    }
+}
+
